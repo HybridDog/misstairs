f3fcb5795bc2f9c452046b4db0b29657231ee814
HybridDog <adresse_test@t-online.de>
2015-04-27 19:14:13 +0200

    :boom:

---
+++ init.lua
@@ -0,0 +1,141 @@
+local function msslab(nam, desc, snds, groups)
+	stairs.register_stair_and_slab(nam, "default:"..nam,
+		groups,
+		{"default_"..nam..".png"},
+		desc.." Stair",
+		desc.." Slab",
+		snds
+	)
+end
+
+local function msslab2(nam, desc, snds, groups, tnam)
+	stairs.register_stair_and_slab(nam, "default:"..nam,
+		groups,
+		{"default_"..tnam..".png"},
+		desc.." Stair",
+		desc.." Slab",
+		snds
+	)
+end
+
+minetest.register_node(":default:mesebrick", {
+	description = "Mese Brick",
+	tiles = {"default_mese_brick.png"},
+	is_ground_content = true,
+	groups = {cracksey=1,level=2},
+	sounds = default.node_sound_stone_defaults(),
+})
+
+--msslab("desert_stone", "Desert Stone", default.node_sound_stone_defaults(), {cracky=3, stone=1})
+--msslab("obsidian", "Obsidian", default.node_sound_stone_defaults(), {cracky=1,level=2})
+msslab2("mese", "Mese", default.node_sound_stone_defaults(), {cracky=1,level=2}, "mese_block")
+--msslab2("desert_stonebrick", "Desert Stone Brick", default.node_sound_stone_defaults(), {cracky=2, stone=1}, "desert_stone_brick")
+--msslab2("sandstonebrick", "Sandstone Brick", default.node_sound_stone_defaults(), {cracky=2}, "sandstone_brick")
+msslab2("mesebrick", "Mese Brick", default.node_sound_stone_defaults(), {cracky=1,level=2}, "mese_brick")
+msslab2("coalblock", "Coal Block", default.node_sound_stone_defaults(), {cracky=3}, "coal_block")
+
+minetest.override_item("default:cobble", {stack_max = 999})
+
+minetest.register_chatcommand("lstuff", {
+	params = "<action>",
+	description = "tell the stuff which's name contains param",
+	privs = {},
+	func = function(name, param)
+		local text = ""
+		for i,_ in pairs(minetest.registered_items) do
+			if string.find(i, param) then
+				text = text..i..", "
+			end
+		end
+		minetest.chat_send_player(name, text)
+	end,
+})
+
+-- disallows torches to be placed into not pointable buildable_to nodes except air
+local torch_place = minetest.registered_items["default:torch"].on_place
+minetest.override_item("default:torch", {
+	on_place = function(itemstack, placer, pt, ...)
+		if pt.above
+		and minetest.get_node(pt.above).name ~= "air" then
+			return
+		end
+		return torch_place(itemstack, placer, pt, ...)
+	end
+})
+
+-- changes papyrus and cactus fcts
+local function get_pseudorandom(pos, seed)
+	return PseudoRandom(math.abs(pos.x+pos.y*3+pos.z*5)+seed)
+end
+
+default.grow_papyrus = function(pos, node)
+	pos.y = pos.y-1
+	local name = minetest.get_node(pos).name
+	if name ~= "default:dirt_with_grass"
+	and name ~= "default:dirt" then
+		return
+	end
+	if not minetest.find_node_near(pos, 3, {"group:water"}) then
+		return
+	end
+	pos.y = pos.y+1
+	local height = 0
+	while node.name == "default:papyrus" and height < 4 do
+		height = height+1
+		pos.y = pos.y+1
+		node = minetest.get_node(pos)
+	end
+	if height == 4
+	or node.name ~= "air" then
+		return
+	end
+
+	if height > 2 then
+		local pr = get_pseudorandom(pos, 11)
+		if pr:next(1,2) == 1 then
+			return
+		end
+	end
+
+	minetest.set_node(pos, {name="default:papyrus"})
+	return true
+end
+
+default.grow_cactus = function(pos, node)
+	if node.param2 ~= 0 then
+		return
+	end
+	pos.y = pos.y-1
+	if minetest.get_item_group(minetest.get_node(pos).name, "sand") == 0 then
+		return
+	end
+	pos.y = pos.y+1
+	local height = 0
+	while node.name == "default:cactus" and height < 4 and node.param2 == 0 do
+		height = height+1
+		pos.y = pos.y+1
+		node = minetest.get_node(pos)
+	end
+	if height == 4
+	or node.name ~= "air" then
+		return
+	end
+
+	if height > 2 then
+		local pr = get_pseudorandom(pos, 10)
+		if pr:next(1,2) == 1 then
+			return
+		end
+	end
+
+	minetest.set_node(pos, {name="default:cactus"})
+	return true
+end
+
+
+
+--[[
+tmp = minetest.registered_nodes["default:leaves"]
+tmp.groups = {snappy=3, leafdecay=3, flammable=2, leaves=1, falling_node=1}
+minetest.register_node(":default:leaves", tmp)
+]]
