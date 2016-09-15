37d1f8ece1b3e4f1a814600c4379f67e4ed767e4
HybridDog <ovvv@web.de>
2016-09-15 10:36:50 +0200

    :cat2:
    M  init.lua

--- init.lua
+++ init.lua
@@ -177,7 +177,13 @@ function table.insert(t, v, n)
 	t[#t+1] = v
 end--]]
 
-
+--[[ this one doesn't work faster
+function unpack(t, i)
+	i = i or 1
+	if t[i] ~= nil then
+		return t[i], unpack(t, i + 1)
+	end
+end]]
 
 
 --[[
